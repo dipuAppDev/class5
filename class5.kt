cd5b08e1bc7d297bc5e665262d1bc67c9e703725
dipuAppDev <dipuappdev@gmail.com>
2023-11-21 13:18:11 +0600

    new file

---
+++ class5.kt
@@ -0,0 +1,186 @@
+fun main(){
+    /**
+     * println (),print(),variable,data type
+     * (int,byte,short,long,float double,char,string,boolean) practice
+     */
+//    println(
+//        "print output and add new line"
+//    )
+//    print(
+//        "print output and add no new line\n"
+//    )
+//   // println("Hello!How are your?\nWhere are you going?")
+//   // println("Hello!How are your?\nWhere are you going?")
+//    val firstName = "Rakibul Islam"
+//    val lastName = "Dipu"
+//    val output = "$firstName $lastName"
+//
+//    println(output)
+//
+//    val intMaxValue = Int.MAX_VALUE
+//    val intMinValue = Int.MIN_VALUE
+//    val intBytesValue = Int.SIZE_BYTES
+//    val intBitsValue = Int.SIZE_BITS
+//
+//    println(intMaxValue)
+//    println(intMinValue)
+//    println(intBytesValue)
+//    println(intBitsValue)
+//
+//
+//    val byteMaxValue = Byte.MAX_VALUE
+//    val byteMinValue = Byte.MIN_VALUE
+//    val byteSizeBytes = Byte.SIZE_BYTES
+//    val byteSizeBits = Byte.SIZE_BITS
+//
+//    println(
+//        byteMaxValue
+//    )
+//    println(
+//        byteMinValue
+//    )
+//    println(
+//        byteSizeBytes
+//    )
+//    println(
+//        byteSizeBits
+//    )
+//    val shortMaxValue = Short.MAX_VALUE
+//    val shortMinValue = Short.MIN_VALUE
+//    val shortSizeBytes = Short.SIZE_BYTES
+//    val shortSizeBits = Short.SIZE_BITS
+//    println(shortMaxValue)
+//    println(shortMinValue)
+//    println(shortSizeBytes)
+//    println(shortSizeBits)
+//
+//    val longMaxValue = Long.MAX_VALUE
+//    val longMinValue = Long.MIN_VALUE
+//    val longSizeBytes = Long.SIZE_BYTES
+
+    /**
+     * Floating point data type
+     */
+//    val num1:Float = 2.4f
+//    val num2:Float = 4.4f
+//    val num1 = 2.4f
+//    val num2 = 4.4f
+//    println(
+//        num1+num2
+//    )
+    /**
+     * Double data type
+     */
+//    val num1:Double = 3.3222
+//    val num2:Double = 3.3222
+//    println("${num1+num2}")
+    /**
+     * Char data type
+     */
+//    val chA : Char = 'A'
+//    println(chA)
+//    val chSmallA : Char = 'a'
+//    println(chSmallA)
+//    val dec:Int = chA.toInt() // decimal value
+//    println(dec)
+
+    /**
+     * Boolean data type
+     */
+//    val isMarried:Boolean = true
+//    val haveChild:Boolean = false
+//    println(isMarried)
+//    println(haveChild)
+    /**
+     * String data type
+     */
+//    val chArray = charArrayOf('R','A','K','I','B')
+//    val st = String(chArray)
+//    println(st)
+//    val name:String
+//    name = "Rakibul"
+//    println(name)
+
+    /**
+     * Operators practice
+     */
+    /* Arithmetic Operator*/
+
+//    //1. +(addition)
+//    val num1 : Int = 10
+//    val num2: Int = 5
+//    val add = num1+num2
+//    println("Addition : ${add}")
+//    //2. -(subtraction)
+//    val sub = num1-num2
+//    println("Subtraction: ${sub}")
+//    //3. *(multiplication)
+//    val mul = num1*num2
+//    println("Multiplication : ${mul}")
+//    //4. /(division)
+//    val div = num1/num2
+//    println("Division : ${div}")
+//    //5. %(modulus)
+//    val mod = num1%num2
+//    println("Modulus : ${mod}")
+
+    /* Assignment Operator*/
+//    var a = 10.0
+//    val b = 5.0
+//    //1. a+=b -> a=a+b
+//     a+=b
+//    println(a)
+//    //2. a-=b -> a=a-b
+//    a-=b
+//    println(a)
+//    //3. a*=b -> a=a*b
+//    a*=b
+//    println(a)
+//    //4. a/=b -> a=a/b
+//    a /= b
+//    println(a)
+//    //5. a%=b -> a=a%b
+//    a%=b
+//    println(a)
+
+//    /**
+//     * unary operators +,-
+//     */
+//    var a = 1
+//    //1. +a "+" unary operator
+//  // println(+a)
+////    //2. -a "-" unary operator
+////    println(-a)
+//    var b = true
+//    //3. !b
+//    println(!b)
+    /**
+     * post and pre increment operator (a++,++a)
+     * post and pre decrement operator (a--,--a)
+     */
+//    //4. a++(increment)
+//    //a++
+//    println(++a)
+//    //5. a--(decrement)
+//    println(a)
+
+    /**
+     * Comparison operators
+     */
+    var a =10
+    var b = 20
+    //1. > (greater than)
+    println(a>b)
+    //2. < (less than)
+    println(a<b)
+    //3. >= (greater than or equal)
+    println(a>=b)
+    //4. <= (less than or equal)
+    println(a<=b)
+    //5. == (equal)
+    println(a==b)
+    //6. != (not equal)
+    println(a!=b)
+
+
+}
